696bf0ad2574a7f4e51b37d3d270e948d72fa279
James <james556674@gmail.com>
2020-10-23 18:55:52 +0800

    feat: add delete record function

--- app.js
+++ app.js
@@ -36,9 +36,37 @@ app.get('/records/new', (req, res) => {
   return res.render('new')
 })
 
+app.get('/records/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Record.findById(id)
+    .lean()
+    .then((record) => res.render('edit', { record }))
+    .catch(error => console.log(error))
+})
+
 app.post('/records', (req, res) => {
   const name = req.body.name
   return Record.create({ name })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
+app.post('/records/:id/edit', (req, res) => {
+  const id = req.params.id
+  const name = req.body.name
+  return Record.findById(id)
+    .then(record => {
+      record.name = name
+      return record.save()
+    })
+    .then(() => res.redirect(`/`))
+    .catch(error => console.log(error))
+})
+
+app.post('/records/:id/delete', (req, res) => {
+  const id = req.params.id
+  return Record.findById(id)
+    .then(record => record.remove())
     .then(() => res.redirect('/'))
     .catch(error => console.log(error))
 })
