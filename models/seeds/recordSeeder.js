4d07c94caa6172b0193f8a4e88f912d55f947c5b
James <james556674@gmail.com>
2020-10-25 14:07:43 +0800

    refactor: modify mongoose config

--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -1,18 +1,8 @@
-const mongoose = require('mongoose')
 const Record = require('../record')
 const records = require('./data/records.json')
-
-mongoose.connect('mongodb://localhost/expense', { useNewUrlParser: true, useUnifiedTopology: true })
-
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error!')
-})
+const db = require('../../config/mongoose')
 
 db.once('open', () => {
-  console.log('mongodb connected!')
-
   Record.create(records.results)
 
   console.log('done')
